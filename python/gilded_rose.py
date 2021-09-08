2041715c465172cd2f5d4c515025157ec6818ab6
Dale Hagglund <@dale.hagglund@gmail.com>
2021-09-07 22:12:27 -0600

    gilded_rose.py: make quality up/down logic more similar
    In preparation for extracting a simple method, interchange a few
    `if` statements so that when `item.quality` is modified it's always
    with the format
    
        if item.quality is in bounds:
        	item.quality = item.quality +/- 1
    
    Also, remove a pointless subtraction that always evaluated to zero,
    and replace with the literal constant zero.
    
    Verified by re-running unit test and text based regression.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,8 +8,8 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.quality > 0:                
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
@@ -26,11 +26,11 @@ class GildedRose(object):
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.quality > 0:
                                 item.quality = item.quality - 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1

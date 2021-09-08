a8d81f90be2c962780048622645d94382e7907f3
Dale Hagglund <@dale.hagglund@gmail.com>
2021-09-07 21:50:10 -0600

    characterization tests: more basic item tests
    Add tests for the quality attribute of basic items.
    
    In addition,
    
    - rename class to be about basic objects to reduce length of test names
    - add a few notes from looking at the requirements file

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -3,23 +3,48 @@ import unittest
 
 from gilded_rose import Item, GildedRose
 
+# notes:
+#
+# - the requirements document says that the quality can never be more than
+#   50, but in in texttest_fixture.py, the Sulfuras items have initial 
+#   quality of 80. So, the requirements have to mean that the quality can 
+#   never be incremented past 50.
 
-class GildedRoseCharacterizationTests(unittest.TestCase):
+
+class BasicItemCharacterizationTests(unittest.TestCase):
     def test_update_doesnt_disturb_name(self):
         item = Item(name="foo", sell_in=5, quality=11)
         g = GildedRose([item])
         g.update_quality()
         self.assertEquals("foo", item.name)
-    def test_update_decreases_time_to_sell_for_basic_item(self):
+    def test_update_decreases_time_to_sell(self):
         item = Item(name="foo", sell_in=5, quality=11)
         g = GildedRose([item])
         g.update_quality()
         self.assertEquals(4, item.sell_in)
-    def test_update_decreases_quality_for_basic_item(self):
+    def test_update_decreases_quality_by_1(self):
         item = Item(name="foo", sell_in=5, quality=11)
         g = GildedRose([item])
         g.update_quality()
         self.assertEquals(10, item.quality)
+
+    def test_quality_drops_by_1_after_last_day_to_sell(self):
+        item = Item(name="foo", sell_in=1, quality=10)
+        g = GildedRose([item])
+        g.update_quality()
+        self.assertEquals(9, item.quality)
+    def test_quality_drops_by_2_after_sell_by_date(self):
+        item = Item(name="foo", sell_in=0, quality=10)
+        g = GildedRose([item])
+        g.update_quality()
+        self.assertEquals(8, item.quality)
+    def test_quality_never_drops_below_0(self):
+        item = Item(name="foo", sell_in=5, quality=0)
+        g = GildedRose([item])
+        g.update_quality()
+        self.assertLessEqual(0, item.quality)
+        
+        
 
         
 if __name__ == '__main__':

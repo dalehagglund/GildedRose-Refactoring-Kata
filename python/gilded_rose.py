3d15a020522695a5de5f43bd54a2b4c17c95907e
Dale Hagglund <@dale.hagglund@gmail.com>
2021-09-08 02:07:36 -0600

    gilded_rose.py: minor refactoring
    make the different blocks of logic, now mutually exclusive for each kind
    of object an if/elif/else chain so they the parallel structure is more
    obvious.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -12,9 +12,7 @@ class GildedRose(object):
                 item.sell_in -= 1
                 if item.sell_in < 0:
                     self.increment_quality(item, 1)
-                continue
-
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 self.increment_quality(item, 1)
                 if item.sell_in < 11:
                     self.increment_quality(item, 1)
@@ -23,16 +21,13 @@ class GildedRose(object):
                 item.sell_in -= 1
                 if item.sell_in < 0:
                     item.quality = 0
-                continue
-
-            if item.name == "Sulfuras, Hand of Ragnaros":
-                # nothing at all
-                continue
-
-            self.decrement_quality(item, 1)
-            item.sell_in -= 1
-            if item.sell_in < 0:
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                pass
+            else:
                 self.decrement_quality(item, 1)
+                item.sell_in -= 1
+                if item.sell_in < 0:
+                    self.decrement_quality(item, 1)
 
     def increment_quality(self, item, delta):
         if item.quality < 50:

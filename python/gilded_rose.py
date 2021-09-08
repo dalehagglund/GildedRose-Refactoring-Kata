b3fd0e300fb0b708ebf71ba1e198fcfca11809fa
Dale Hagglund <@dale.hagglund@gmail.com>
2021-09-07 22:26:59 -0600

    gilded_rose.py: reduce duplicate quality logic
    - introduce the `GildedRose.adjust_quality` method to adjust quality
      either up or down while enforcing upper and lower bounds.
    
      NOTE: if the delta would move quality beyond either the upper or lower
      limit, quality is not changed at all. Should it be adjusted as far as
      possible?
    
    - use `adjust_quality` to eliminate all but one explicit adjustment of
      quality in the main `update_quality` function. The remaining case will
      be handled in the next commit.
    
    Tested with regression and unit tests. This change is a bit dangerous
    without better unit tests, though.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,31 +9,30 @@ class GildedRose(object):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.name != "Sulfuras, Hand of Ragnaros":
-                    if item.quality > 0:                
-                        item.quality = item.quality - 1
+                    self.adjust_quality(item, -1)
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            self.adjust_quality(item, +1)
                         if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            self.adjust_quality(item, +1)
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.name != "Sulfuras, Hand of Ragnaros":
-                            if item.quality > 0:
-                                item.quality = item.quality - 1
+                            self.adjust_quality(item, -1)
                     else:
                         item.quality = 0
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.adjust_quality(item, +1)
+
+    def adjust_quality(self, item, delta):
+        if 0 < item.quality < 50:
+            item.quality += delta
 
 
 class Item:

680945d63449907d5352f680c0fba7b7515e0cf0
Dale Hagglund <@dale.hagglund@gmail.com>
2021-09-07 21:31:29 -0600

    characterization tests: basic items
    introduce a few characterization tests for the behaviour of basic
    items.  more to come...

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,12 +4,22 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+class GildedRoseCharacterizationTests(unittest.TestCase):
+    def test_update_doesnt_disturb_name(self):
+        item = Item(name="foo", sell_in=5, quality=11)
+        g = GildedRose([item])
+        g.update_quality()
+        self.assertEquals("foo", item.name)
+    def test_update_decreases_time_to_sell_for_basic_item(self):
+        item = Item(name="foo", sell_in=5, quality=11)
+        g = GildedRose([item])
+        g.update_quality()
+        self.assertEquals(4, item.sell_in)
+    def test_update_decreases_quality_for_basic_item(self):
+        item = Item(name="foo", sell_in=5, quality=11)
+        g = GildedRose([item])
+        g.update_quality()
+        self.assertEquals(10, item.quality)
 
         
 if __name__ == '__main__':
